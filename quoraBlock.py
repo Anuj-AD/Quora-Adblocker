0595f6d3819f3cedbebed307dc41f03d2730dfb6
Anuj Dadhwal <anujdadhwal3012@gmail.com>
2020-04-29 13:55:51 +0530

    added a few comments

--- quoraBlock.py
+++ quoraBlock.py
@@ -10,7 +10,7 @@ from random import randint
 url = 'https://www.quora.com/' #our website
 path = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s' #path of chrome
 webbrowser.get(path).open(url) #open website on chrome
-time.sleep(3)
+time.sleep(3)#reduce this to low value like 0.2 if internet is fast
 image = pag.screenshot("ss.png") #take screenshot
 image = cv2.imread("ss.png")
 #cv2.imshow("Screenshot",imutils.resize(image, width = 400)) #to check the screenshot
@@ -18,7 +18,7 @@ image = cv2.imread("ss.png")
 logoLocation = pag.locateOnScreen('logo.png', confidence = 0.9)
 logoPoint = pag.center(logoLocation)
 pag.moveTo(logoPoint)
-time.sleep(3)
+time.sleep(3)#reduce this to low value like 0.2 if internet is fast
 timeout = time.time() + 60 #1 minute
 while True:
     rand1 = randint(600,800)/1000   
@@ -36,7 +36,7 @@ while True:
         pag.moveTo(likePoint)
         pag.move(540,0)
         pag.click()
-        time.sleep(2)
+        time.sleep(2)#reduce this to low value like 0.2 if internet is fast
         cutLocation = pag.locateOnScreen('cut.png', confidence = 0.9)
         cutPoint = pag.center(cutLocation)
         pag.moveTo(cutPoint)
@@ -49,7 +49,7 @@ while True:
         pag.moveTo(likePoint)
         pag.move(485,-20)
         pag.click()
-        time.sleep(2)
+        time.sleep(2)#reduce this to low value like 0.2 if internet is fast
         cutLocation = pag.locateOnScreen('cut.png', confidence = 0.9)
         cutPoint = pag.center(cutLocation)
         pag.moveTo(cutPoint)
